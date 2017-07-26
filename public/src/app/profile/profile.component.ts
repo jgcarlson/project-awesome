65eeff2a8361e75ce6a8b61c5f8b2b1be140d265
jonathan <jgcarlson@outlook.com>
2017-07-26 11:48:18 -0700

    adding add product form to vendor profile

--- public/src/app/profile/profile.component.ts
+++ public/src/app/profile/profile.component.ts
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
   ngOnInit() {
   }
+
+  currentUser:any;
 
 }
