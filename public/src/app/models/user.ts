512e74757b83b1b53220374e83c92dc74f1be4f4
Scott <marden.scott@gmail.com>
2017-07-26 12:53:44 -0700

    Added registration validations(front end)

--- public/src/app/models/user.ts
+++ public/src/app/models/user.ts
@@ -2,6 +2,7 @@ export class User {
   constructor(
     public first_name:string = '',
     public last_name:string = '',
+	public alias:string='',
     public email:string = '',
     public password:string = '',
     public confirm:string = '',
