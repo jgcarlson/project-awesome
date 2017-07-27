e8204ad94b720e6385ecf3a833945e2de5abd2d6
jonathan <jgcarlson@outlook.com>
2017-07-27 14:45:36 -0700

    prettified basket. kinda.

--- public/src/app/basket/basket.component.ts
+++ public/src/app/basket/basket.component.ts
@@ -14,36 +14,36 @@ export class BasketComponent implements OnInit {
   constructor(private _omniService: OmniService) { }
 
   ngOnInit() {
-	this.currentUser = JSON.parse(localStorage.getItem("currentUser"))
-	console.log("Current User: ", this.currentUser)
-	this.getBasket(this.currentUser.user.id)
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"))
+    console.log("Current User: ", this.currentUser)
+    this.getBasket(this.currentUser.user.id)
   }
 
   getBasket(id){
-	  this._omniService.get_basket(id)
-	  .then( user => {
-		  this.userBasket = user.basket
-	  })
-	  .catch( err => {
-		  console.log(err)
-	  })
+    this._omniService.get_basket(id)
+    .then( user => {
+      this.userBasket = user.basket
+    })
+    .catch( err => {
+      console.log(err)
+    })
 
   }
 
   remove_from_basket(product){
-	  let body = {
-		  userId: this.currentUser.user.id,
-		  product: product
-	  }
-	  console.log("Body: ", body)
-	  this._omniService.remove_from_basket(body)
-	  .then( data => {
-		  console.log(data)
-		  this.getBasket(this.currentUser.user.id)
-	  })
-	  .catch( err => {
-		  console.log(err)
-	  })
+    let body = {
+      userId: this.currentUser.user.id,
+      product: product
+    }
+    console.log("Body: ", body)
+    this._omniService.remove_from_basket(body)
+    .then( data => {
+      console.log(data)
+      this.getBasket(this.currentUser.user.id)
+    })
+    .catch( err => {
+      console.log(err)
+    })
   }
 
 }
